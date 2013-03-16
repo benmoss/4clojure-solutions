03fc50ce5162ad51b09405c774231ac8f2554c75
Ben Moss <ben@mossity.com>
2013-03-16 16:43:12 -0400

    Refactored.

--- src/fourclojure/50.clj
+++ src/fourclojure/50.clj
@@ -2,19 +2,7 @@
   (:use midje.sweet))
 
 (defn solution [mixed]
-  (loop [strings []
-         symbols []
-         numbers []
-         vectors []
-         head (first mixed)
-         tail (rest mixed)]
-    (cond
-      (nil? head) (filter not-empty [strings symbols numbers vectors])
-      :else (case (str (.getClass head))
-              "class java.lang.String" (recur (conj strings head) symbols numbers vectors (first tail) (rest tail))
-              "class clojure.lang.Keyword" (recur strings (conj symbols head) numbers vectors (first tail) (rest tail))
-              "class java.lang.Long" (recur strings symbols (conj numbers head) vectors (first tail) (rest tail))
-              "class clojure.lang.PersistentVector" (recur strings symbols numbers (conj vectors head) (first tail) (rest tail))))))
+  (vals (group-by type mixed)))
 
 (fact
   (= (set (solution [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
