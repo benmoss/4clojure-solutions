c45d7ef1693d1614c571075c3cdcc668d3b58ade
Ben Moss <ben@mossity.com>
2013-04-20 12:42:59 -0400

    Add missing paren on 50

--- src/fourclojure/50.clj
+++ src/fourclojure/50.clj
@@ -7,4 +7,4 @@
 (fact
   (= (set (solution [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
   (= (set (solution [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]})
-  (= (set (solution [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})
+  (= (set (solution [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]}))
